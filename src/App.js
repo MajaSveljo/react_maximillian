0bad269bdf58874b250d63b650f6fc8288fc99f1
nemanja <nemanja.stankovic@extraordinary.rs>
2019-01-16 16:20:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Maja", age: 25},
+      { name: "Dragan", age: 26},
+      { name: "Pepsi", age: 2},      
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: being annoying</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
+
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work now?'))
   }
 }
 

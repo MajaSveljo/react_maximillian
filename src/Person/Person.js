172444c084ac36c0b2e4a54809b5a042d18e0817
nemanja <nemanja.stankovic@extraordinary.rs>
2019-01-22 13:37:33 +0100

    Preparation for sec 5 lec 67 - before ejecting

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import Radium from 'radium';
 
 import './Person.css';
 
 const person = (props) => {
-    // može i kod iz person.css-a, ali taj kod je globalan. AKO želim da promene budu scope-ovane u komponenti
-    // ILI ako želim da ih dinamički menjam onda ovde koristim Radium
-    
-    //in styles components we define the styles as JS objects and tie them to components as properties
-    // between the ' ' is javascript PROPERTY name, since it's a string it's valid
-    const style = {
-        '@media (min-width: 500px)' : {
-            width: '450px'
-        }
-    };
     return (
-        <div className="Person" style={style}>
+        <div className="Person">
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
@@ -23,7 +12,4 @@ const person = (props) => {
     )
 };
 
-export default Radium(person);
-
-//for pseudo selectors, wrapping components in Radium is enough.
-// BUT for some, eg. some animations and media queries, whole app has to be wrapped in StyleRoot Radium object
+export default person;

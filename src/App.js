53e3159a42234e0fd63f3611dd5895882e5c7918
nemanja <nemanja.stankovic@extraordinary.rs>
2019-01-22 16:38:23 +0100

    Ejected, configured webpack, used modules for localised styles sec 5 lec 67

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'; 
-import './App.css';
+import classes from './App.css';
+// classes refers to App.css, the whole document
 import Person from './Person/Person';
 
 class App extends Component {
@@ -74,19 +75,20 @@ class App extends Component {
         style.backgroundColor = 'red';
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); 
+      assignedClasses.push(classes.red); 
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); 
+      assignedClasses.push(classes.bold); 
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
+      {/* written like this App refers to .App in App.css. Here it's imported as property */}
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working</p>      
+        <p className={assignedClasses.join(' ')}>This is really working</p>      
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name</button>
